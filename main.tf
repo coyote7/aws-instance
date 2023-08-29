d9491a1e01585927f024adef952b10a28c29f527
coyote7 <odeny7@gmail.com>
2023-08-29 20:03:53 +0300

    creating an EC2 instance v-1

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=4.16"
+    }
+  }
+}
+provider "aws" {
+  region  = "us-east-2"
+  profile = "ronte"
+}
+
+resource "aws_instance" "test-server" {
+  ami              = ""
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.test-sgrp.id]
+  user_data              = <<-EOF
+               #!/bin/bash
+               echo "Welcome to Our   Portal">index.html
+               nohup busybox http -f -p  8080
+                EOF
+
+}
+resource "aws_security_group" "test-sgrp" {
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
